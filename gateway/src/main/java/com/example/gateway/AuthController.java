84590eb2d0e7d60cf67cb7aa84dc08c7a46a60f6
NguyenTung <tnkoko123@gmail.com>
2026-05-02 04:31:20 +0700

    change redirect url

---
+++ gateway/src/main/java/com/example/gateway/AuthController.java
@@ -0,0 +1,29 @@
+package com.example.gateway;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Gateway is working!";
+    }
+
+    @GetMapping("/auth/google/init")
+    public void initOAuth(
+            @RequestParam String redirectUri,
+            HttpServletRequest req,
+            HttpServletResponse res) throws IOException {
+
+        req.getSession().setAttribute("redirect_uri", redirectUri);
+
+        res.sendRedirect("/oauth2/authorization/google");
+    }
+}
