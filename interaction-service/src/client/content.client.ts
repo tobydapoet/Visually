ea941a8994f219170467ccbf6099a621e9c2709f
NguyenTung <tnkoko123@gmail.com>
2026-05-04 23:03:37 +0700

    verify 404 content

--- interaction-service/src/client/content.client.ts
+++ interaction-service/src/client/content.client.ts
@@ -1,5 +1,5 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
 import { ContentDto } from './dto/user-response.dto';
 import { ContentServiceType } from 'src/enums/ContentType';
@@ -9,21 +9,42 @@ export class ContentClient {
   constructor(private readonly http: HttpService) {}
 
   async getPost(postId: number): Promise<ContentDto> {
-    const url = `${process.env.CONTENT_SERVICE_URL}/post/${postId}`;
-    const res = await firstValueFrom(this.http.get<ContentDto>(url));
-    return res.data;
+    try {
+      const url = `${process.env.CONTENT_SERVICE_URL}/post/${postId}`;
+      const res = await firstValueFrom(this.http.get<ContentDto>(url));
+      return res.data;
+    } catch (error: any) {
+      if (error.response?.status === 404) {
+        throw new NotFoundException('Post not found');
+      }
+      throw error;
+    }
   }
 
   async getShort(shortId: number): Promise<ContentDto> {
-    const url = `${process.env.CONTENT_SERVICE_URL}/short/${shortId}`;
-    const res = await firstValueFrom(this.http.get<ContentDto>(url));
-    return res.data;
+    try {
+      const url = `${process.env.CONTENT_SERVICE_URL}/short/${shortId}`;
+      const res = await firstValueFrom(this.http.get<ContentDto>(url));
+      return res.data;
+    } catch (error: any) {
+      if (error.response?.status === 404) {
+        throw new NotFoundException('Short not found');
+      }
+      throw error;
+    }
   }
 
   async getStory(storyId: number): Promise<ContentDto> {
-    const url = `${process.env.CONTENT_SERVICE_URL}/story/${storyId}`;
-    const res = await firstValueFrom(this.http.get<ContentDto>(url));
-    return res.data;
+    try {
+      const url = `${process.env.CONTENT_SERVICE_URL}/story/${storyId}`;
+      const res = await firstValueFrom(this.http.get<ContentDto>(url));
+      return res.data;
+    } catch (error: any) {
+      if (error.response?.status === 404) {
+        throw new NotFoundException('Story not found');
+      }
+      throw error;
+    }
   }
 
   async verifyTarget(
